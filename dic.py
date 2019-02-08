3f60e37af6c03dd126d1c56cd10e01dfc0595de8
Kim Minwoo <43179854+davidkmw0810@users.noreply.github.com>
2019-02-08 13:25:54 +0900

    Update dic.py

--- dic.py
+++ dic.py
@@ -1,23 +1,20 @@
 from selenium import webdriver
 
 # word = input('what word do you want to find?')
-# country = int(input('choose dictionary 1.English 2.Korean 3.Hanja 4.Japanese 5.Chinese 6.French 7.Spanish 8.German'))
+# country = input('choose dictionary 1.English 2.Chinese 3.French 4.Spanish')
 
 word = 'pomme'
-country = 6
+country = '2'
 driver = webdriver.Chrome('chromedriver.exe')
 
 # naver 사전
-driver.get('https://dict.naver.com/')
-
-# 사전별로 링크 클릭해서 들어가기
-dics = driver.find_elements_by_class_name("Nnav_cell")
-dics[country].click()
-
-# 검색
-driver.find_element_by_id('ac_input').send_keys(word)
-driver.find_element_by_class_name('btn_search').click()
-
-# 긁어오기
-mean = driver.find_element_by_class_name('fnt_k05').text
-print(mean)
+if country == '1':
+    driver.get('https://endic.naver.com/?sLn=kr')
+    driver.find_element_by_id(ac_input).send_keys(word)
+    
+elif country == '2':
+    driver.get('https://zh.dict.naver.com/#/main')
+elif country == '3':
+    driver.get('https://dict.naver.com/frkodict/#/main')
+elif country == '4':
+    driver.get('https://dict.naver.com/eskodict/#/main')
