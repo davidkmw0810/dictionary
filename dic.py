6cde14aa709bdd03297dee3070e5e869bd6e8f3a
Kim Minwoo <43179854+davidkmw0810@users.noreply.github.com>
2019-02-08 13:58:18 +0900

    headless 옵션

--- dic.py
+++ dic.py
@@ -1,11 +1,16 @@
 from selenium import webdriver
 
-# word = input('what word do you want to find?')
-# country = input('choose dictionary 1.English 2.Chinese 3.French 4.Spanish')
+word = input('what word do you want to find?')
+country = input('choose dictionary 1.English 2.Chinese 3.French 4.Spanish')
 
-word = 'Bloquer'
-country = '3'
-driver = webdriver.Chrome('chromedriver.exe')
+# headless 옵션 설정
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu")
+
+# Chrome 드라이버 불러오기
+driver = webdriver.Chrome('chromedriver.exe', chrome_options=options)
 
 # naver 사전
 if country == '1':
@@ -28,7 +33,6 @@ else:
         driver.get('https://dict.naver.com/frkodict/#/main')
     elif country == '4':
         driver.get('https://dict.naver.com/eskodict/#/main')
-
     driver.find_element_by_id('ac_input').send_keys(word)
     driver.find_element_by_xpath('//*[@id="searchArea"]/button').click()
 
